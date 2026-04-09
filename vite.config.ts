8136a533aaa28cd5eb53f7667efc79d96cd6c548
Gleb Kornilov <esplete@gmail.com>
2026-04-09 22:31:08 +0300

    fix: deploy errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-oxc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/some-app/',
+  base: '/some-app/src',
   plugins: [react()],
   resolve: {
     alias: {
